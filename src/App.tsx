7f26e307a4cddd8259344a54245143e240520082
Benedikt Jazdzewski <jazdzewski@b1-systems.de>
2021-10-11 13:42:18 +0200

    Mask for changing user Password
    Password Mask with validation in German and English, language switch is now intergradet into the TopBar and can be chaged via browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
-import './App.css';
-
-import React, { Suspense } from 'react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { RecoilRoot } from 'recoil';
 
-import { TopBar } from '@b1-systems/react-components';
+import { Toastyfier, TopBar, useToasty } from '@b1-systems/react-components';
 import { Link, Toolbar } from '@mui/material';
 
-import LangSwitch from './components/langSwitch';
 import ChangePasswort from './components/passwortTool/changePasswort';
+
+function InnerApp() {
+    const {toasty} = useToasty();
+    const {t, i18n} = useTranslation();
+
+    return (
+        <RecoilRoot>
+            <TopBar
+                menuEntries={[]}
+                logoutAction={() => toasty.success('Logout complete!')}
+                applicationTitle={
+                    <Link href='' color='inherit' underline='hover'>
+                        {t('Change password')}
+                    </Link>
+                }
+                notificationHistory={{
+                    pastNotifications: 'Past Notifications',
+                    noNotificationsYet: 'No notifications yet',
+                    createdAtFormat: (v: number) => new Date(v).toString(),
+                }}
+                languageMenu={{
+                    entries: [
+                        {key: 'de', display: 'Deutsch'},
+                        {key: 'en', display: 'English'},
+                    ],
+                    onLanguageChange: (key: string) => {
+                        const msg =
+                            key === 'en'
+                                ? 'Switched to English'
+                                : 'Sprache auf Deutsch gestellt';
+                        toasty.success(msg);
+                        i18n.changeLanguage(key);
+                    },
+                    currentLanguage: 'en',
+                }}
+            />
+            <Toolbar />
+            <ChangePasswort></ChangePasswort>
+        </RecoilRoot>
+    );
+}
 
 export default function App() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <RecoilRoot>
-                <TopBar
-                    menuEntries={[]}
-                    logoutAction={() => console.log('Logout')}
-                    applicationTitle={
-                        <Link href='' color='inherit' underline='hover'>
-                            Titel
-                        </Link>
-                    }
-                    notificationHistory={false}
-                />
-                <Toolbar />
-                <LangSwitch></LangSwitch>
-                <ChangePasswort></ChangePasswort>
-            </RecoilRoot>
-        </Suspense>
+        <Toastyfier position={'bottom-right'} gutter={8}>
+            <InnerApp />
+        </Toastyfier>
     );
 }
